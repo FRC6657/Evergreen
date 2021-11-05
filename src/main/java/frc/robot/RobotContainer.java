29cc860c8373f5d358907750eede840c2ee88eb4
Andrew Card <mrsaltinecracker1@gmail.com>
2021-11-05 16:04:43 -0700

    added subsystems to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,33 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Agitator;
+import frc.robot.subsystems.Blinkin;
+import frc.robot.subsystems.ColorSensor;
+import frc.robot.subsystems.ControlPanel;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.IntakeCamera;
+import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.PDP;
 
 public class RobotContainer {
-  
+
   //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   //private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final Agitator m_agitator = new Agitator();
+  private final Blinkin m_blinkin = new Blinkin();
+  private final ColorSensor m_colorSensor = new ColorSensor();
+  private final ControlPanel m_controlPanel = new ControlPanel();
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final Intake m_intake = new Intake();
+  private final IntakeCamera m_intakeCamera = new IntakeCamera();
+  private final Lift m_lift = new Lift();
+  private final Limelight m_limelight = new Limelight();
+  private final PDP m_PDP = new PDP();
+  
 
   public RobotContainer() {
     configureButtonBindings();
