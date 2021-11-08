8626a751b2ed0d149301bdce58700c6efa73a65e
Andrew Card <mrsaltinecracker1@gmail.com>
2021-11-07 21:22:32 -0800

    please?

--- src/main/java/frc/robot/commands/changeCamera.java
+++ src/main/java/frc/robot/commands/changeCamera.java
@@ -7,13 +7,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeCamera;
 
-public class ChangeCamera extends CommandBase {
+public class changeCamera extends CommandBase {
   
   //
 
   private final IntakeCamera mIntakeCamera;
 
-  public ChangeCamera(IntakeCamera pIntakeCamera) {
+  public changeCamera(IntakeCamera pIntakeCamera) {
     mIntakeCamera = pIntakeCamera;
     addRequirements(mIntakeCamera);
   }
